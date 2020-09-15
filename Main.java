25f1f007eba28da5e805cdef62c580f74769df87
Girlc7442 <girlc7442@wrdsb.ca>
2020-09-15 14:10:52 +0000

    Finished with comments

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *Takes 2 numbers and determines which is bigger
+ * @author Cole G.
  */
 public class Main {
 
@@ -9,7 +11,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user for an integer
+    System.out.println("Please enter an integer:");
+    //Get the integer from the user
+    int userNumber1 = input.nextInt();
     
+    //ask the user for another integer
+    System.out.println("Please enter another integer:");
+    //Get the second integer from the user
+    int userNumber2 = input.nextInt();
+
+    //Check to see which number is larger
+    if(userNumber1 > userNumber2){
+      //if the first number is larger than the second
+      System.out.println("The biggest number is " + userNumber1 + ".");
+    }else if(userNumber1 < userNumber2){
+      //if the second number is larger than the first
+      System.out.println("The biggest number is " + userNumber2 + ".");
+    }else{
+      //if the numbers are equal
+      System.out.println("The numbers are equal.");
+    }
   }
 }
